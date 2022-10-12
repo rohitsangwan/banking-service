5c9385ba03d6976df9720fbd24597efcd202fac5
Rohit Sangwan <rohitsangwan@197NODMB41169.local>
2022-10-12 16:51:46 +0530

    Made insertDetailsForOnBoarding transactional

--- src/main/java/com/bankingservice/banking/services/AccountService.java
+++ src/main/java/com/bankingservice/banking/services/AccountService.java
@@ -17,6 +17,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Account Service
@@ -67,6 +68,7 @@ public class AccountService {
      * @throws InsertionFailedException
      * @throws UserIdNotFoundException
      */
+    @Transactional
     public OnBoardResponseDTO insertDetailsForOnBoarding(OnBoardRequestDTO onBoardRequestDTO) throws
             UserIdNotFoundException, InsertionFailedException, ServiceCallException {
         try {
